5c08a3854b0a3d4b2c29e69d1ba40e481b64f17d
jorgequinteros <jorge.quinteros@ing.uchile.cl>
2016-05-24 18:42:50 -0400

    instance creator casi listo

--- src/InstanceCreator.java
+++ src/InstanceCreator.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by jorge on 24-05-16.
  */
@@ -6,4 +8,50 @@ public class InstanceCreator {
     private int n = (int) Math.pow(2, 20);
     private int[] alphabetSize = {2, 4, 8, 16, 32, 64, 128, 256};
 
+    private char[][] alphabet = new char[8][];
+
+    public InstanceCreator() {
+
+        for (int i = 0; i < this.alphabetSize.length; i++) {
+            this.alphabet[i] = new char[this.alphabetSize[i]];
+            for (int j = 0; j < this.alphabetSize[i]; j++) {
+                this.alphabet[i][j] = (char) (j + 300);
+            }
+        }
+    }
+
+    public char[][] getAlphabet() {
+        return this.alphabet;
+    }
+
+    public int[] getAlphabetSize() {
+        return this.alphabetSize;
+    }
+
+    public String alphabetToString(int exp) {
+        String string = "";
+        for (char character : this.alphabet[exp - 1]) {
+            string = string + character;
+        }
+        return string;
+    }
+
+    public String generateInstance(int exp) {
+        int step = n / alphabetSize[exp - 1];
+        String instance = "";
+
+        Random random = new Random();
+
+        for (int i = 0; i < n; i++) {
+            if (i % step == 0) {
+                instance = instance + alphabet[exp - 1][i / alphabetSize[exp - 1]];
+                // revisar esto
+            }
+            else {
+                instance = instance + alphabet[exp - 1][random.nextInt(alphabetSize[exp - 1])];
+            }
+        }
+
+        return instance;
+    }
 }
